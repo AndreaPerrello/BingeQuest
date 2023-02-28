353b9c06a029bc464521961abd64416e0c324b60
Perrello, Andrea <andrea.perrello@nttdata.com>
2023-02-28 12:47:04 +0100

    FIX: App run

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pathlib
 
 from iotech.microservice import startup
 
-# Create the cache folder
-pathlib.Path('.mycache').mkdir(parents=True, exist_ok=True)
-# Create the app
-app = startup.cmd_create_app(["core", "controller", "Controller"])
+if __name__ == '__main__':
+    # Create the cache folder
+    pathlib.Path('.mycache').mkdir(parents=True, exist_ok=True)
+    # Create the app
+    app = startup.cmd_create_app(["core", "controller", "Controller"])
+
 
