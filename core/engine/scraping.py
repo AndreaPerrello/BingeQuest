38ecdb3a16276e9d73b84a0646d63166ce83d9a9
Perrello, Andrea <andrea.perrello@nttdata.com>
2023-02-28 16:09:51 +0100

    TRY: Scraper timeout

--- core/engine/scraping.py
+++ core/engine/scraping.py
@@ -7,15 +7,15 @@ _base_kwargs = dict(timeout=10)
 
 def get(url: str, cloud: bool = True, *args, **kwargs) -> requests.Response:
     if cloud:
-        cloud_scraper = cloudscraper.create_scraper()
-        return cloud_scraper.get(url, *args, timeout=10, **kwargs)
+        cloud_scraper = cloudscraper.create_scraper(timeout=10)
+        return cloud_scraper.get(url, *args, **kwargs)
     return requests.get(url, *args, **kwargs)
 
 
 def post(url: str, cloud: bool = True, *args, **kwargs) -> requests.Response:
     if cloud:
-        cloud_scraper = cloudscraper.create_scraper()
-        return cloud_scraper.post(url, *args, timeout=10, **kwargs)
+        cloud_scraper = cloudscraper.create_scraper(timeout=10)
+        return cloud_scraper.post(url, *args, **kwargs)
     return requests.post(url, *args, **kwargs)
 
 
