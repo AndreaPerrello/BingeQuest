93f860ba4297fad45ee99a97ddab3c7a3da0f8be
Perrello, Andrea <andrea.perrello@nttdata.com>
2023-02-28 16:21:11 +0100

    TRY: Use requests

--- core/engine/scraping.py
+++ core/engine/scraping.py
@@ -2,21 +2,19 @@ import bs4
 import cloudscraper
 import requests
 
-_base_kwargs = dict(timeout=10)
 
-
-def get(url: str, cloud: bool = True, *args, **kwargs) -> requests.Response:
+def get(url: str, cloud: bool = False, *args, **kwargs) -> requests.Response:
     if cloud:
-        cloud_scraper = cloudscraper.create_scraper(delay=10)
+        cloud_scraper = cloudscraper.create_scraper()
         return cloud_scraper.get(url, *args, **kwargs)
-    return requests.get(url, *args, **kwargs)
+    return requests.get(url, *args, verify=False, **kwargs)
 
 
-def post(url: str, cloud: bool = True, *args, **kwargs) -> requests.Response:
+def post(url: str, cloud: bool = False, *args, **kwargs) -> requests.Response:
     if cloud:
-        cloud_scraper = cloudscraper.create_scraper(delay=10)
+        cloud_scraper = cloudscraper.create_scraper()
         return cloud_scraper.post(url, *args, **kwargs)
-    return requests.post(url, *args, **kwargs)
+    return requests.post(url, *args, verify=False, **kwargs)
 
 
 def get_soup(url: str, *args, **kwargs) -> bs4.BeautifulSoup:
