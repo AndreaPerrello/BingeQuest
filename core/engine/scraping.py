3a8ab18cbdb65db6212ef3dc6c724eb916ef687f
Perrello, Andrea <andrea.perrello@nttdata.com>
2023-02-28 16:25:50 +0100

    ADD: Response log

--- core/engine/scraping.py
+++ core/engine/scraping.py
@@ -2,19 +2,28 @@ import bs4
 import cloudscraper
 import requests
 
+import logging
+LOGGER = logging.getLogger(__name__)
+
 
 def get(url: str, cloud: bool = False, *args, **kwargs) -> requests.Response:
     if cloud:
         cloud_scraper = cloudscraper.create_scraper()
-        return cloud_scraper.get(url, *args, **kwargs)
-    return requests.get(url, *args, verify=False, **kwargs)
+        result = cloud_scraper.get(url, *args, **kwargs)
+    else:
+        result = requests.get(url, *args, verify=False, **kwargs)
+    LOGGER.info(result.text)
+    return result
 
 
 def post(url: str, cloud: bool = False, *args, **kwargs) -> requests.Response:
     if cloud:
         cloud_scraper = cloudscraper.create_scraper()
-        return cloud_scraper.post(url, *args, **kwargs)
-    return requests.post(url, *args, verify=False, **kwargs)
+        result = cloud_scraper.post(url, *args, **kwargs)
+    else:
+        result = requests.post(url, *args, verify=False, **kwargs)
+    LOGGER.info(result.text)
+    return result
 
 
 def get_soup(url: str, *args, **kwargs) -> bs4.BeautifulSoup:
