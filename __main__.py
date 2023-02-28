bdc541122216d0fe48cf3bc0553840d6620023cc
Perrello, Andrea <andrea.perrello@nttdata.com>
2023-02-28 13:17:30 +0100

    FIX: App run

---
+++ __main__.py
@@ -0,0 +1,8 @@
+import pathlib
+
+from iotech.microservice import startup
+
+# Create the cache folder
+pathlib.Path('.mycache').mkdir(parents=True, exist_ok=True)
+# Create the app
+app = startup.cmd_create_app(["core", "controller", "Controller"])
