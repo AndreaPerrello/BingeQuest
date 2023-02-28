32a733510f994bad7b809c1b2b0b4a726aafba14
Perrello, Andrea <andrea.perrello@nttdata.com>
2023-02-28 16:07:59 +0100

    TRY: Add timeout

--- core/engine/scraping.py
+++ core/engine/scraping.py
@@ -2,18 +2,20 @@ import bs4
 import cloudscraper
 import requests
 
+_base_kwargs = dict(timeout=10)
+
 
 def get(url: str, cloud: bool = True, *args, **kwargs) -> requests.Response:
     if cloud:
         cloud_scraper = cloudscraper.create_scraper()
-        return cloud_scraper.get(url, *args, **kwargs)
+        return cloud_scraper.get(url, *args, timeout=10, **kwargs)
     return requests.get(url, *args, **kwargs)
 
 
 def post(url: str, cloud: bool = True, *args, **kwargs) -> requests.Response:
     if cloud:
         cloud_scraper = cloudscraper.create_scraper()
-        return cloud_scraper.post(url, *args, **kwargs)
+        return cloud_scraper.post(url, *args, timeout=10, **kwargs)
     return requests.post(url, *args, **kwargs)
 
 
